f47825616412c6768216857874d463bee43687ba
nesllewr <llewrones@gmail.com>
2020-03-28 19:51:38 +0900

    1204

---
+++ 1204.py
@@ -0,0 +1,12 @@
+import sys
+
+fp = open('input1204.txt','r')
+T = int(fp.readline())
+for test_case in range(1, 1 + T):
+    score = [ 0 for i in range(101)]
+    num = fp.readline().strip('\n')
+    #print("{} num {} ".format(test_case, num))
+    now = list(map(int,fp.readline().split()))
+    for j in range(1000):
+        score[now[j]] += 1
+    print(score.index(max(score)))
