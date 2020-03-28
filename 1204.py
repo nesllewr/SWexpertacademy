ec733fb3005275e41fe5fcd2dcabeedee6799d7d
nesllewr <llewrones@gmail.com>
2020-03-28 20:15:44 +0900

    1204 pass

--- 1204.py
+++ 1204.py
@@ -9,4 +9,6 @@ for test_case in range(1, 1 + T):
     now = list(map(int,fp.readline().split()))
     for j in range(1000):
         score[now[j]] += 1
-    print(score.index(max(score)))
+    res_list = [i for i, value in enumerate(score) if value == max(score)]
+    print("#{} {}".format(test_case,max(res_list)))
+#    print(score.index(max(score)))
